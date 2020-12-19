634b50899cc191edf0378dca9077a7766f20a38e
Justas Raisys <jraisys@gmail.com>
2020-12-19 21:08:30 +0200

    Pabaigtas

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -36,14 +36,16 @@
             this.pies_output = new System.Windows.Forms.RichTextBox();
             this.pies_input = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.skaic_prid = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.mas_val = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // skaic_mygtukas
             // 
-            this.skaic_mygtukas.Location = new System.Drawing.Point(6, 48);
+            this.skaic_mygtukas.Location = new System.Drawing.Point(87, 48);
             this.skaic_mygtukas.Name = "skaic_mygtukas";
             this.skaic_mygtukas.Size = new System.Drawing.Size(75, 38);
             this.skaic_mygtukas.TabIndex = 0;
@@ -55,24 +57,25 @@
             // 
             this.skaic_input.Location = new System.Drawing.Point(6, 22);
             this.skaic_input.Name = "skaic_input";
-            this.skaic_input.Size = new System.Drawing.Size(129, 20);
+            this.skaic_input.Size = new System.Drawing.Size(204, 20);
             this.skaic_input.TabIndex = 1;
             // 
             // skaic_rezul
             // 
             this.skaic_rezul.Location = new System.Drawing.Point(6, 92);
             this.skaic_rezul.Name = "skaic_rezul";
-            this.skaic_rezul.Size = new System.Drawing.Size(129, 20);
+            this.skaic_rezul.Size = new System.Drawing.Size(204, 20);
             this.skaic_rezul.TabIndex = 2;
             // 
             // skaic_label
             // 
             this.skaic_label.AutoSize = true;
-            this.skaic_label.Location = new System.Drawing.Point(141, 99);
+            this.skaic_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.skaic_label.ForeColor = System.Drawing.Color.Red;
+            this.skaic_label.Location = new System.Drawing.Point(216, 96);
             this.skaic_label.Name = "skaic_label";
-            this.skaic_label.Size = new System.Drawing.Size(35, 13);
+            this.skaic_label.Size = new System.Drawing.Size(0, 16);
             this.skaic_label.TabIndex = 3;
-            this.skaic_label.Text = "label1";
             // 
             // pies_mygtukas
             // 
@@ -101,16 +104,28 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.mas_val);
+            this.groupBox1.Controls.Add(this.skaic_prid);
             this.groupBox1.Controls.Add(this.skaic_rezul);
             this.groupBox1.Controls.Add(this.skaic_mygtukas);
             this.groupBox1.Controls.Add(this.skaic_input);
             this.groupBox1.Controls.Add(this.skaic_label);
-            this.groupBox1.Location = new System.Drawing.Point(36, 43);
+            this.groupBox1.Location = new System.Drawing.Point(36, 30);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(324, 144);
+            this.groupBox1.Size = new System.Drawing.Size(459, 157);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Skaičiavimas";
+            // 
+            // skaic_prid
+            // 
+            this.skaic_prid.Location = new System.Drawing.Point(6, 48);
+            this.skaic_prid.Name = "skaic_prid";
+            this.skaic_prid.Size = new System.Drawing.Size(75, 38);
+            this.skaic_prid.TabIndex = 4;
+            this.skaic_prid.Text = "Pridėti į masyvą";
+            this.skaic_prid.UseVisualStyleBackColor = true;
+            this.skaic_prid.Click += new System.EventHandler(this.skaic_prid_Click);
             // 
             // groupBox2
             // 
@@ -119,16 +134,26 @@
             this.groupBox2.Controls.Add(this.pies_output);
             this.groupBox2.Location = new System.Drawing.Point(36, 204);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(324, 317);
+            this.groupBox2.Size = new System.Drawing.Size(459, 317);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Eglutė";
+            // 
+            // mas_val
+            // 
+            this.mas_val.Location = new System.Drawing.Point(6, 127);
+            this.mas_val.Name = "mas_val";
+            this.mas_val.Size = new System.Drawing.Size(71, 24);
+            this.mas_val.TabIndex = 5;
+            this.mas_val.Text = "Išvalyti masyvą";
+            this.mas_val.UseVisualStyleBackColor = true;
+            this.mas_val.Click += new System.EventHandler(this.mas_val_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 549);
+            this.ClientSize = new System.Drawing.Size(503, 549);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -152,6 +177,8 @@
         private System.Windows.Forms.TextBox pies_input;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button skaic_prid;
+        private System.Windows.Forms.Button mas_val;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,19 +12,93 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+
+        public int[] masiv = new int[10];
+        public int m = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void skaic_mygtukas_Click(object sender, EventArgs e)
+        private void skaic_prid_Click(object sender, EventArgs e)
         {
 
+                if (int.TryParse(skaic_input.Text, out masiv[m]))
+                {
+                    m++;
+                    skaic_rezul.Text = masiv[0] + " + ";
+                    
+                    for (int i = 1; i < m; i++)
+                    {
+                        skaic_rezul.AppendText(masiv[i] + " + ");
+
+                }
+                    if (m == 10)
+                    {
+                        skaic_prid.Enabled = false;
+                    }
+                }
+            skaic_input.Clear();
+        }
+        private void skaic_mygtukas_Click(object sender, EventArgs e)
+        {
+            if (m < 10)
+            {
+                skaic_label.Text = "Masyve trūksta skaičių";
+            }
+            else
+            {
+                int i = 0;
+                int suma = 0;
+                while (true)
+                {
+                    suma += masiv[i];
+                    i++;
+                    if (i == m) break;
+                }
+                skaic_rezul.Text = "Suma yra: " + suma;
+            }
+        }
+
+        private void mas_val_Click(object sender, EventArgs e)
+        {
+            skaic_rezul.Clear();
+            skaic_input.Clear();
+            skaic_label.Text = "";
+            m = 0;
+            skaic_prid.Enabled = true;
         }
 
         private void pies_mygtukas_Click(object sender, EventArgs e)
         {
-
+            int size;
+            int i = 0;
+            int star = 1;
+            int space;
+            if(int.TryParse(pies_input.Text, out size) && size > 0)
+            {
+                pies_output.Clear();
+                space = size;
+                do
+                {
+                    for (int n = 1; n < space; n++)
+                    {
+                        pies_output.AppendText(" ");
+                    }
+                    for (int n = 0; n < star; n++)
+                    {
+                        pies_output.AppendText("* ");
+                    }
+                    pies_output.AppendText("\n");
+                    space--;
+                    star++;
+                    i++;
+                } while (i < size);
+            }
         }
+
+
     }
 }
